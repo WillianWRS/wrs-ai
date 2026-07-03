fae76a8a41a9ed0510b9eac579cc4010182d088e
WILLIAN\willi <willian-scabora@hotmail.com>
2026-07-03 19:57:56 -0300

    initial commit

---
+++ src/test/java/wrs/ai/AiApplicationTests.java
@@ -0,0 +1,13 @@
+package wrs.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
